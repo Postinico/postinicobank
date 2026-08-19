9470c64eb1d6f186ea85589ebe5864cd4f46db76
GPOSTINICO <guilherme.postinico@passarela.com.br>
2026-08-18 22:32:13 -0300

    adiciona: facotry

---
+++ src/postinico-bank/Cartoes/CartaoBlack.cs
@@ -0,0 +1,20 @@
+﻿using postinico_bank.Enums;
+using postinico_bank.Interfaces;
+
+namespace postinico_bank.Cartoes
+{
+    public class CartaoBlack : ICartao
+    {
+        public bool ObterTipo(TipoCartao tipo) => tipo == TipoCartao.Black;
+
+        public void Debitar(decimal valor)
+        {
+            Console.WriteLine($"[BLACK] Débito de R$ {valor:F2} realizado com sucesso.");
+        }
+
+        public void Creditar(decimal valor)
+        {
+            Console.WriteLine($"[BLACK] Crédito de R$ {valor:F2} adicionado à fatura.");
+        }
+    }
+}
